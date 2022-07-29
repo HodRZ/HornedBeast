8d6061fafbcad6def30b1ee86a6804ad4de97fe6
HodRZ <hodaifa.radwan@gmail.com>
2022-07-29 11:25:20 +0300

    Fix modal image sizing

--- src/components/SelectedBeast.js
+++ src/components/SelectedBeast.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import { Image } from "react-bootstrap";
 
 class MyModal extends React.Component {
     render() {
         return (
             <Modal show={this.props.show}
-                size="lg"
                 aria-labelledby="contained-modal-title-vcenter"
                 centered>
                 <Modal.Header>
@@ -15,7 +15,7 @@ class MyModal extends React.Component {
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <img src={this.props.selectedAnimal.image_url} alt={this.props.selectedAnimal.name} />
+                    <Image src={this.props.selectedAnimal.image_url} alt={this.props.selectedAnimal.name} fluid />
                     <p>
                         {this.props.selectedAnimal.description}
                     </p>
